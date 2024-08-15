a0b41058e6d289e659699a0e4c7881e357bd166f
lichfiet <sortaminty@gmail.com>
2024-08-14 20:33:54 -0600

    added some content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -23,8 +23,31 @@ const About = function () {
                 </h1>
                 <div className="p-2">
                     <div id="work-lightspeed" className="">
-                        <p className="text-base font-base">Meow meow meow</p>
+                        <p className="indent-3 mb-2">
+                            I've always been interested in technology, starting
+                            with my family's first computer, where I played a lot of classic computer games like
+                            Roller Coaster Tycoon, and an early Roblox. Later, I would have my first
+                            exposure to web development, modifying free HTML templates for an SEO project. I ended up taking 
+                            a simple Javascript class 
+                            in middle school, learned how to deploy Minecraft servers on docker with my dad, and have enjoyed 
+                            exploring new technologies in my current software support role ever since.
+                        </p>
+                        <p className="indent-3 mb-2">
+                            When I'm not coding, you can find me anxiously watering my plants, rock climbing, playing video games, or
+                            spending time with my girlfriend and our wonderful pets. I also enjoy a good iced coffee when the opportunity 
+                            to have one presents itself. When I do have coffee, I aim to drink it whilst
+                            on vacation, or somewhere far from home. I love to travel, and have recently been to Seattle, Washington which
+                            I wholeheartedly recommend anyone <i>drive to</i> and visit.
+                        </p>
                     </div>
+                </div>
+                <h1 className="text-xl font-semibold">Music</h1>
+                <div className="p-2">
+                    <p className="indent-3 mb-5">
+                        I think music is very important to me. It's accompanied many emotions in my life and I think I have quite the taste. Below is a 
+                        window into my soul, and also my favorite playlist of pre 2000's music I regularly listen to.
+                    </p>
+                    <iframe src="https://open.spotify.com/embed/playlist/0G7BnxP1X1M827QDhg0AB5?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                 </div>
             </div>
         </div >
--- src/pages/workExperience.jsx
+++ src/pages/workExperience.jsx
@@ -19,36 +19,36 @@ const WorkExperience = function () {
 
                         </div>
                         <div className="p-2">
-                            <h1 className="text-l font-medium">Lightspeed DMS LLC.</h1>
-                            <p className="text-sm md:text-base font-light">Customer Support Analyst II</p>
+                            <h1 className="text-l font-bold">Lightspeed DMS LLC.</h1>
+                            <p className="text-sm md:text-base font-medium">Customer Support Analyst II</p>
                             <p className="text-sm md:text-base font-light italic">Aug. 2022 - Now</p>
-                            <p className="p-2 text-sm md:text-base">
+                            <p className="p-2 text-sm md:text-base mb-3">
                                 <b>Responsibilities:</b> Provide front-end application support, including the creation and execution of
                                 complex SQL queries for data validation and troubleshooting. Responsible for detailed bug reporting and
-                                contributing to technical documentation. Engage in a wide range of tasks to enhance system reliability
-                                and customer satisfaction.
+                                contributing to technical documentation. Provide support to our largest clients, including training,
+                                technical advice, and in depth software troubleshooting.
                             </p>
-                            <p className="text-sm md:text-base font-light">Customer Support Analyst I</p>
+                            <p className="text-sm md:text-base font-medium">Customer Support Analyst I</p>
                             <p className="text-sm md:text-base font-light italic">Aug. 2022 - Now</p>
                             <p className="p-2 text-sm md:text-base">
-                                <b>Responsibilities:</b> Provide front-end application support for the Accounting and Integrations 
+                                <b>Responsibilities:</b> Provide front-end application support for the Accounting and Integrations
                                 the DMS offerred. Author SQL queries to validate data and troubleshoot issues.
                             </p>
                         </div>
                     </div>
-                    <div id="work-starbucks" className="grid" style={{ gridTemplateColumns: "1fr 50fr" }}>
+                    <div id="work-starbucks" className="grid mt-5" style={{ gridTemplateColumns: "1fr 50fr" }}>
                         <div className="bg-black w-0.5 mt-3 mb-3">
                         </div>
                         <div className="p-2">
-                            <h1 className="text-l font-medium">Starbucks Coffee Company</h1>
-                            <p className="text-sm md:text-base font-light">Customer Support Analyst II</p>
+                            <h1 className="text-l font-bold">Starbucks Coffee Company</h1>
+                            <p className="text-sm md:text-base font-medium">Customer Support Analyst II</p>
                             <p className="text-sm md:text-base font-light italic">Aug. 2021 - Aug. 2022</p>
                             <p className="p-2 text-sm md:text-base">
-                                <b>Responsibilities:</b> Work for extended periods of time in a fast-paced environment with a 
-                                focus on maintaining consistency and efficiency. Communicate with customers in a welcoming way 
-                                to foster a sense of community and inclusivity amongst our location’s customer-base. Operate a 
-                                complex POS system with many options, while simultaneously interpreting the colloquial terms 
-                                used to describe our products and options. Work in a team setting where cooperation and 
+                                <b>Responsibilities:</b> Work for extended periods of time in a fast-paced environment with a
+                                focus on maintaining consistency and efficiency. Communicate with customers in a welcoming way
+                                to foster a sense of community and inclusivity amongst our location’s customer-base. Operate a
+                                complex POS system with many options, while simultaneously interpreting the colloquial terms
+                                used to describe our products and options. Work in a team setting where cooperation and
                                 communication is key to ensure efficiency and quality of the goods produced.
                             </p>
                         </div>
