c4d1bf4ae858d1c710e57037d4893b5344cc7c5f
lichfiet <sortaminty@gmail.com>
2024-08-19 22:30:14 -0600

    redesigning projects pahe

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,18 +1,106 @@
+import { useState, useRef, useEffect } from 'react';
+
 const Projects = function () {
-    
-    return (
-        <>
-            <div className="container p-6 mx-auto">
-                <h1 className="font-bold text-2xl">File Explorer</h1>
-                <a href="https://files.trevorlichfield.com" target="_blank" rel="noopener noreferrer"><p className="underline">files.trevorlichfield.com</p></a>
-                <h1 className="mt-4 italic font-semibold hidden md:block">Live Preview</h1>
+    const [activeProject, setActiveProject] = useState(null);
+    const [activeProjectName, setActiveProjectName] = useState(null);
+
+
+    const FileExplorerRef = useRef(null);
+    const FileExplorer = () => {
+        return <>
+            <div className="flex justify-center items-center content-center" >
+                <iframe className="w-full hidden sm:block iframe-mobile"
+                    src="https://files.trevorlichfield.com" title="File Explorer" allow="accelerometer; autoplay; clipboard-write; encrypted-media"></iframe>
+                <img className="block sm:hidden " src="./file explorer.png"></img>
             </div>
-                <div className="flex p-4 mx-auto justify-center items-center content-center">
-                    <iframe className="w-full hidden sm:block iframe-mobile" 
-                     src="https://files.trevorlichfield.com" title="File Explorer" allow="accelerometer; autoplay; clipboard-write; encrypted-media"></iframe>
-                    <img className="block sm:hidden" src="./file explorer.png"></img>
+        </>
+    };
+
+    const Project2 = () => {
+        return <>
+                <img className={`self-center items-center mx-auto`} src='https://via.placeholder.com/1100x600'></img>
+        </>
+    }
+
+    const Project3 = () => {
+        return <>
+                <img className={`self-center items-center mx-auto`} src='https://via.placeholder.com/1100x600'></img>
+        </>
+    }
+
+    const ProjectSelector = (project) => {
+        if (activeProject === project) {
+            return
+        } else if (project === "File Explorer") {
+            setActiveProject(<FileExplorer />);
+            setActiveProjectName("File Explorer");
+
+        } else if (project === "Project 2") {
+            setActiveProject(<Project2 />);
+            setActiveProjectName("Project 2");
+        } else if (project === "Project 3") {
+            setActiveProject(<Project3 />);
+            setActiveProjectName("Project 3");
+        }
+    }
+
+    const navButtons = [
+        {
+            name: "File Explorer",
+            button: 
+                <button onClick={() => ProjectSelector("File Explorer")} >
+                    <div className={`p-5 m-2 bg-neutral-200 hover:bg-neutral-300 rounded-xl ${activeProjectName === 'File Explorer' ? 'bg-neutral-100 shadow-xl animate-bounce' : ''}`} >
+                        <h1 className="font-semibold text-lg">File Explorer</h1>
+                        <img className={`${activeProjectName !== 'File Explorer' ? '' : 'hidden'}`} src='./explorer.png'></img>
+                    </div>
+                </button>
+        },
+        {
+            name: "Project 2",
+            button: 
+                <button onClick={() => ProjectSelector("Project 2")} >
+                    <div className={`p-5 m-2 bg-neutral-200 hover:bg-neutral-300 rounded-xl ${activeProjectName === 'Project 2' ? 'bg-neutral-100 shadow-xl animate-bounce' : ''}`} >
+                        <h1 className="font-semibold text-lg">Project 2</h1>
+                        <img className={`${activeProjectName !== 'Project 2' ? '' : 'hidden'}`} src='https://via.placeholder.com/600x300'></img>
+                    </div>
+                </button>
+        },
+        {
+            name: "Project 3",
+            button:
+                <button onClick={() => ProjectSelector("Project 3")} >
+                    <div className={`p-5 m-2 bg-neutral-200 hover:bg-neutral-300 rounded-xl ${activeProjectName === 'Project 3' ? 'bg-neutral-100 shadow-xl animate-bounce' : ''}`} >
+                        <h1 className="font-semibold text-lg">Project 3</h1>
+                        <img className={`${activeProjectName !== 'Project 3' ? '' : 'hidden'}`} src='https://via.placeholder.com/600x300'></img>
+                    </div>
+                </button>
+        }
+    ]
+
+
+    return (
+
+        useEffect(() => {
+            ProjectSelector("File Explorer");
+        }, []),
+
+        <>
+            <div className="projects-container container mx-auto block md:grid" style={{ gridTemplateColumns: "1fr 3fr" }}>
+                <nav className='hidden flex-col justify-center me-4 my-4 md:flex' >
+                    {
+                        navButtons.sort((btn) => {
+                            if (btn.name === activeProjectName) return -1; // Move a to the front
+                            if (btn.name !== activeProjectName) return 1; // Move b to the front
+                            return 0; // Keep the original order
+                        }).map((btn) => btn.button)
+                    }
+                </nav>
+                <div className="rounded-xl mr-4">
+                    <h1 className="font-bold text-5xl text-center">{activeProjectName}</h1>
+                    {activeProject}
                 </div>
-            <div className="container p-6 max-w-sm mx-auto bg-neutral-200 rounded-xl shadow-xl mb-6">
+            </div>
+            <div className="container p-6 max-w-sm mx-auto bg-neutral-100 rounded-xl shadow-xl mb-6">
                 <div className="p-2 block md:grid" style={{ gridTemplateColumns: "3fr 1fr" }}>
                     <div className="p-2">
                         <h1 className="text-xl font-bold mb-1 underline">About This Project.</h1>
@@ -44,17 +132,17 @@ const Projects = function () {
                 <div className="sm:flex md:grid md:grid-cols-3 px-6 mx-auto">
                     <button className="bg-neutral-800 m-2 p-2 rounded-xl min-w-50">
                         <a className="text-white hover:text-gray-300 md:block" href="https://github.com/lichfiet/file-explorer-web" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i> Frontend (Main Page)
+                            <i className="fab fa-github"></i> Frontend (Main Page)
                         </a>
                     </button>
                     <button className="bg-neutral-800 m-2 p-2 rounded-xl min-w-50">
                         <a className="text-white hover:text-gray-300 md:block" href="https://github.com/lichfiet/file-explorer-backend" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i> Backend
+                            <i className="fab fa-github"></i> Backend
                         </a>
                     </button>
                     <button className="bg-neutral-800 m-2 p-2 rounded-xl min-w-50">
                         <a className="text-white hover:text-gray-300 md:block" href="https://github.com/lichfiet/file-explorer-infra" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github"></i> Infrastructure
+                            <i className="fab fa-github"></i> Infrastructure
                         </a>
                     </button>
                 </div>
