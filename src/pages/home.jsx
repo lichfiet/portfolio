f376fae0e12b4d29e19ef7fb390f4c7e08335b65
Trevor Lichfield <sortaminty@gmail.com>
2025-01-25 22:03:30 -0700

    changing up homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,33 +6,33 @@ const HomePage = function () {
 
     return (
         <div className="flex-grow overflow-auto container mx-auto my-auto justify-center content-center px-0 lg:px-28 md:px-4 md:pb-28">
-            <div className="block md:grid grid-cols-2">
+            <div className="block lg:grid grid-cols-2">
                 <div className="content-center p-6">
                     <div className="flex content-center justify-center">
                         <img src="https://via.placeholder.com/250" alt="profile picture" />
                     </div>
-                    <div className="flex justify-center">
-                        <ul className="p-4 text-left">
-                            <li className="font-medium text-base lg:text-lg">Hey! <i className="font-light">My name is,</i></li>
-                            <li className="text-xl lg:text-3xl italic hover:not-italic"><b>Trevor Lichfield</b></li>
-                            <li className="font-light text-base lg:text-lg italic underline">Fullstack SWE & DevOps Engineer</li>
+                    <div className="justify-center">
+                        <ul className="m-16 text-left p-4">
+                            <li className="font-medium text-base lg:text-lg items-end">
+                                <i className="font-light pr-1">Hi, i'm</i>
+                                <p className="text-xl lg:text-3xl italic inline"><b>Trevor Lichfield,</b>
+                                <p className='inline text-base font-light'> a Cloud & Devops Engineer, utilizing my development experience, and love of operations, to reduce the workload of maintaining and deploying software products.</p></p></li>
                         </ul>
                     </div>
                 </div>
                 <div className="flex justify-center content-center mx-5 md:mx-10">
-                    <div className="p-6 my-auto dark:bg-neutral-800 bg-neutral-200 rounded-lg shadow-2xl flex flex-col h-fit">
-                        <h1 className="md:text-base lg:text-lg font-bold text-center">
-                            Where am I?
-                        </h1>
-                        <p className="indent-3 mb-2 text-sm lg:text-base">
-                            Well, first, thanks for stopping by! This website is supposed to serve as a home for all my projects and
-                            things I've worked on over my years so you can take a look at em'. 
+                    <div className="p-6 my-auto flex flex-col h-fit">
+                        <p className='text-5xl p-2 text-center text-black dark:text-white'>
+                            <a href="./projects" onClick={(e) => { }}>/<i>projects</i></a>
                         </p>
-                        <p className='p-1 text-center text-black dark:text-white'>
-                            <a href="./projects" onClick={(e) => { }} className="underline font-semibold">My Projects</a>
+                        <p className='text-5xl p-2 text-center text-black dark:text-white'>
+                            <a href="./about" onClick={(e) => { }}>/<i>about</i></a>
                         </p>
-                        <p className='p-1 text-center text-black dark:text-white'>
-                            <a href="./about" onClick={(e) => { }} className="underline font-semibold">Read About Me</a>
+                        <p className='text-5xl p-2 text-center text-black dark:text-white'>
+                            <a href="https://github.com/lichfiet" onClick={(e) => { }}>/<i>github</i></a>
+                        </p>
+                        <p className='text-6xl p-2 text-center text-black dark:text-white'>
+                            <a href="./about" onClick={(e) => { }}>/<i>linkedin</i></a>
                         </p>
                     </div>
                 </div>
